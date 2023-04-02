d1ec5b14fae380a0f1667e1d9a4027c45a77eab9
Khasehemwy <BuluyuAzir@foxmail.com>
2023-04-02 16:20:41 +0800

    [Update] skybox shader support hdr

--- Test_PBR/shader_skybox.h
+++ Test_PBR/shader_skybox.h
@@ -3,6 +3,10 @@
 
 class Renderer_Skybox :public Renderer {
 public:
+	bool isHDR = false;
+	float exposure = 1.0f;
+	float gamma = 2.2f;
+
 	Texture_Cube* texture_cube;
 	virtual void VS(vertex_t* v1, vertex_t* v2, vertex_t* v3);
 	virtual color_t PS(vertex_t* v);
@@ -37,6 +41,25 @@ inline color_t Renderer_Skybox::PS(vertex_t* v)
 	tex_coord.y = -tex_coord.y;
 
 	//texture_cube->Write(tex_coord, color_t(1.0));
+	vector_t color = texture_cube->Read(tex_coord);
 
-	return texture_cube->Read(tex_coord);
+	color.x = pow(color.x, gamma);
+	color.y = pow(color.y, gamma);
+	color.z = pow(color.z, gamma);
+
+	if (isHDR) {
+		//color = vector_t(1.0) - 
+		//	vector_t(exp(-color.x * exposure),
+		//		exp(-color.y * exposure), 
+		//		exp(-color.z * exposure));
+
+		color = color / (color + vector_t(1.0f));
+	}
+
+	color.x = pow(color.x, 1.0f / gamma);
+	color.y = pow(color.y, 1.0f / gamma);
+	color.z = pow(color.z, 1.0f / gamma);
+
+	color_t color_use(color.x, color.y, color.z, 1);
+	return color_use;
 }
